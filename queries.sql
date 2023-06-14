c38d69401b12ed6b17db644faa8ddfe241fe6614
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-06-14 13:57:53 +0200

    Add multiple tables and data

--- queries.sql
+++ queries.sql
@@ -98,3 +98,41 @@ FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
 GROUP BY species;
 
+--------------------------------------------------------------------
+
+SELECT animals.name, owners.full_name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+SELECT animals.name
+FROM animals
+JOIN species ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+SELECT o.full_name, a.name
+FROM owners o
+LEFT JOIN animals a ON o.id = a.owner_id;
+
+SELECT s.name, COUNT(a.id) AS count
+FROM species s
+LEFT JOIN animals a ON s.id = a.species_id
+GROUP BY s.name;
+
+SELECT a.name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
+JOIN species s ON a.species_id = s.id
+WHERE o.full_name = 'Jennifer Orwell' AND s.name = 'Digimon';
+
+SELECT a.name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
+WHERE a.escape_attempts = 0 AND o.full_name = 'Dean Winchester';
+
+SELECT o.full_name, COUNT(a.id) AS count
+FROM owners o
+JOIN animals a ON o.id = a.owner_id
+GROUP BY o.full_name
+ORDER BY count DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -9,3 +9,33 @@ weight_kg DECIMAL
 
 ALTER TABLE animals
 ADD species VARCHAR(60);
+
+CREATE TABLE owners(
+	id SERIAL NOT NULL,
+	full_name VARCHAR(60),
+	age INTEGER,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+	id SERIAL NOT NULL,
+	name VARCHAR(70),
+	PRIMARY KEY(id)
+);
+
+ALTER TABLE 
+animals
+ADD PRIMARY KEY(id);
+
+ALTER 
+TABLE animals
+DROP species;
+SELECT * from animals;
+
+ALTER TABLE animals
+ADD species_id INT
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owner_id INT 
+REFERENCES owners(id);
