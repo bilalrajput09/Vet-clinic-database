6d4e41dceaf84b75a9d78843d793c4a7c216bf71
Omar Farouq <ofarouq310@gmail.com>
2023-06-19 15:06:24 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -203,6 +203,10 @@ GROUP BY s.name
 ORDER BY num_visits DESC
 LIMIT 1;
 
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+
 
 
 
